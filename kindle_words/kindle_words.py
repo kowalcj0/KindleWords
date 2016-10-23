3c7bd4a687e8b69e1df28525138e21e4b43cbad5
kowalcj0 <janusz@justyoyo.com>
2016-10-23 01:37:19 +0100

    filter out US & GB words and optimize the way clipped words are found

--- kindle_words/kindle_words.py
+++ kindle_words/kindle_words.py
@@ -1,26 +1,45 @@
 from string import digits
+import enchant
+from itertools import zip_longest
 
-def open_clippings(file_name):
+
+def grouper(iterable, n, fillvalue=None):
+    """
+    Will groupd lines in an opened file into chunks of n lines.
+    SRC: http://stackoverflow.com/a/5845141
+    """
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def open_clippings(filename):
     correct = set()
-    with open(file_name, 'r') as clippings:
+    N = 5
+    with open(filename, 'r') as infile:
+        clippings = set()
+        for lines in grouper(infile, N, ''):
+            assert len(lines) == N
+            clippings.add(lines[3].strip().lower())
         for line in clippings:
-            if line != '==========\r\n':
-                if line.strip():
-                    words = line.split()
-                    if len(words) <= 2:
-                        word = ' '.join(words)
-                        no_specials = word.translate({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`‘~-=_+/[] ·ˈˌ–—„\"“”"})
-                        if no_specials.strip():
-                            no_eses = no_specials.replace('’s', '')
-                            no_eses = no_eses.replace('\'s', '')
-                            no_backticks = no_eses.replace('’', '\'')
-                            no_end_qoutes = no_backticks.rstrip("'")
-                            remove_digits = str.maketrans('', '', digits)
-                            no_digits = no_end_qoutes.translate(remove_digits)
-                            lower = no_digits.lower().strip()
-                            no_end_qoutes = lower.rstrip("'")
-                            no_end_qoutes = no_end_qoutes.replace('  ', ' ')
-                            correct.add(no_end_qoutes)
+            if len(line.split()) <= 2:
+                no_specials = line.translate({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`‘~-=_+/[] ·ˈˌ–—„\"“”"})
+                if no_specials.strip():
+                    no_eses = no_specials.replace('’s', '')
+                    no_eses = no_eses.replace('\'s', '')
+                    no_backticks = no_eses.replace('’', '\'')
+                    no_end_qoutes = no_backticks.rstrip("'")
+                    remove_digits = str.maketrans('', '', digits)
+                    no_digits = no_end_qoutes.translate(remove_digits)
+                    lower = no_digits.lower().strip()
+                    no_end_qoutes = lower.rstrip("'")
+                    no_end_qoutes = no_end_qoutes.replace('  ', ' ')
+                    us = enchant.Dict("en_US")
+                    gb = enchant.Dict("en_GB")
+                    clean_words = no_end_qoutes.split()
+                    is_us = all([us.check(w) for w in clean_words])
+                    is_gb = all([gb.check(w) for w in clean_words])
+                    if is_us or is_gb:
+                        correct.add(no_end_qoutes)
 
     sort = sorted(correct)
     print(sort)
