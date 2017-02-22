585a0aef58dba7d95a149fcff889149ce5fd49c6
kowalcj0 <janusz@justyoyo.com>
2017-02-22 15:45:47 +0000

    added placeholder text

--- app/forms.py
+++ app/forms.py
@@ -3,4 +3,8 @@ from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 
 class WordsForm(FlaskForm):
-    words = TextAreaField('words', validators=[DataRequired()])
+    placeholder =  "Enter a list of comma-separated english words, e.g.: bird, kindle, candles"
+    words = TextAreaField(
+        'words',
+        validators=[DataRequired()],
+        render_kw={"placeholder": placeholder})
