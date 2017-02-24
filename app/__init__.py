f9502d0555d4f0e6ddee7d117eb3af60bcf268ef
kowalcj0 <janusz@justyoyo.com>
2017-02-24 13:46:42 +0000

    load DB into memory as dict

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,35 @@
+# -*- coding: utf-8 -*-
+import time
+import sqlite3
+
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+
+def load_into_dict_sqlite():
+    res = {}
+    start = time.time()
+    conn = sqlite3.connect('./app/static/wn31-slim3.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    rs = c.execute('''SELECT word, pos, us_ipa, definition
+                      FROM words
+                      ORDER BY word''')
+    for r in rs:
+        if r['word'] not in res:
+            res[r['word']] = {}
+            res[r['word']]['us_ipa'] = None
+            res[r['word']]['origin'] = None
+            res[r['word']]['definitions'] = []
+        if not res[r['word']]['us_ipa']:
+            res[r['word']]['us_ipa'] = r['us_ipa']
+        res[r['word']]['definitions'].append({'pos': r['pos'], 'definition': r['definition']})
+    end = time.time()
+    print('Successfully loaded {} word definitions from SQLite DB into Dict in: {}'.format(len(res), end - start))
+    return res
+
+MEMDB = load_into_dict_sqlite()
 
 from app import views
