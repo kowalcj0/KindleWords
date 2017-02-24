865036422407fae92d37a52c5cac3b23f410fe2d
kowalcj0 <janusz@justyoyo.com>
2017-02-24 15:16:19 +0000

    renamed us_ipa to ipa

--- app/__init__.py
+++ app/__init__.py
@@ -14,17 +14,17 @@ def load_into_dict_sqlite():
     conn = sqlite3.connect('./app/static/wn31-slim3.db')
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    rs = c.execute('''SELECT word, pos, us_ipa, definition
+    rs = c.execute('''SELECT word, pos, ipa, definition
                       FROM words
                       ORDER BY word''')
     for r in rs:
         if r['word'] not in res:
             res[r['word']] = {}
-            res[r['word']]['us_ipa'] = None
+            res[r['word']]['ipa'] = None
             res[r['word']]['origin'] = None
             res[r['word']]['definitions'] = []
-        if not res[r['word']]['us_ipa']:
-            res[r['word']]['us_ipa'] = r['us_ipa']
+        if not res[r['word']]['ipa']:
+            res[r['word']]['ipa'] = r['ipa']
         res[r['word']]['definitions'].append({'pos': r['pos'], 'definition': r['definition']})
     end = time.time()
     print('Successfully loaded {} word definitions from SQLite DB into Dict in: {}'.format(len(res), end - start))
